0e24bd447863c376813e60ae63fe663d00c7f87e
diego <diegohm1598@gmail.com>
2022-05-31 13:15:37 +0200

    Cambiando deps

--- model/model.py
+++ model/model.py
@@ -1,3 +1,4 @@
+from tensorflow_hub import KerasLayer
 from keras.models import model_from_json
 
 
@@ -6,7 +7,7 @@ def load_model_bert():
     model_json = model_json_r.read()
     model_json_r.close()
 
-    model = model_from_json(model_json)
+    model = model_from_json(model_json, custom_objects={'KerasLayer': KerasLayer})
     model.load_weights("model_3/model_2.h5")
 
     return model
